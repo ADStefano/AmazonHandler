a76907c1d8cc41fe9ae93e5db6e3a6f88415ead0
Ângelo <angelo.pdistefano@gmail.com>
2025-10-08 23:56:32 -0300

    fix: ajustado parâmetro da função upload

--- tests/s3handler_tests/upload_test.go
+++ tests/s3handler_tests/upload_test.go
@@ -1,48 +1,61 @@
 package s3handler_test
 
 import (
-	"github.com/ADStefano/AmazonHandler/s3handler"
 	"context"
 	"errors"
+	"os"
 	"testing"
+
+	"github.com/ADStefano/AmazonHandler/s3handler"
 )
 
 type TestUpload struct {
+	TestName       string
 	TestBucketName string
 	Prefix         string
-	Path           string
+	Filename       string
+	File           *os.File
 	ExpectedOutput bool
 	ExpectedError  error
 }
 
+var mockFile *os.File
+
 var testUploadCases = []TestUpload{
 	{
+		TestName:       "Success - upload without prefix",
 		TestBucketName: "test",
 		Prefix:         "",
-		Path:           "/home/angelo/Documentos/Programação/exemplo.html",
+		Filename:       "exemplo.html",
+		File:           mockFile,
 		ExpectedOutput: true,
 		ExpectedError:  nil,
 	},
 	{
+		TestName:       "Success",
 		TestBucketName: "test",
 		Prefix:         "uploads",
-		Path:           "/home/angelo/Documentos/Programação/exemplo.html",
+		Filename:       "exemplo.html",
+		File:           mockFile,
 		ExpectedOutput: true,
 		ExpectedError:  nil,
 	},
 	{
+		TestName:       "Fail - Entity Too Large",
 		TestBucketName: "entity-too-large",
 		Prefix:         "uploads",
-		Path:           "/home/angelo/Documentos/Programação/exemplo.html",
+		Filename:       "exemplo.html",
+		File:           mockFile,
 		ExpectedOutput: false,
 		ExpectedError:  s3handler.ErrEntityTooLarge,
 	},
 }
 
 func TestUploads(t *testing.T) {
+
 	for _, testCase := range testUploadCases {
-		t.Run(testCase.Path, func(t *testing.T) {
-			output, err := mockClient.UploadS3(testCase.TestBucketName, testCase.Prefix, testCase.Path, context.Background())
+		t.Run(testCase.TestName, func(t *testing.T) {
+			output, err := mockClient.UploadS3(testCase.TestBucketName, testCase.Prefix, testCase.Filename, testCase.File, context.Background())
 
 			if output != testCase.ExpectedOutput {
 				t.Errorf("Output esperado %v, recebido %v", testCase.ExpectedOutput, output)
